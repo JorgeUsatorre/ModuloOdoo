f36454c88e0691443a5f818af7fa7333a4e0362d
JorgeUsatorre <usatorre1210@gmail.com>
2024-03-11 00:46:49 +0100

    extra-addons

---
+++ extra-addons/openacademy/models/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class TestModel(models.Model):
+    _name = "test_model"
+    _description = "Modelo de prueba"
+
+    name = fields.Char(string="Nombre")
+    description = fields.Text(string="Numero de telefono")
+
+
+
