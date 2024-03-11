4bd418f062dd4b11f23da9cc9af1b68355a6e08d
JorgeUsatorre <usatorre1210@gmail.com>
2024-03-11 03:33:26 +0100

    extra-addons

--- extra-addons/openacademy/__manifest__.py
+++ extra-addons/openacademy/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -26,6 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
+        'data/datos.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
